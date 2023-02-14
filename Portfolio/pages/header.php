6c631df8af86b8ed6114dfbd68d682af8924dcba
Theoode <118999054+Theoode@users.noreply.github.com>
2023-02-14 15:55:55 +0100

    Add files via upload

---
+++ Portfolio/pages/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+</head>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+</head>
+
+<header>
+            <h1>Portfolio</h1>
+            <hr style="width: 450px; height: 0.070em;">
+        <nav class="navbar">
+            <a href="#container_photo"><h3>A propos</h3></a>
+            <a href="#projet"><h3>Mes projets</h3></a>
+            <a href="mailto:theoquennehenpro@gmail.com"><h3>Contact</h3></a>
+        </nav>
+        <nav id="logo">
+            <img src="../medias/github.png" height="25px" width="25px">
+            <img src="../medias/linkedin.png" height="25px" width="25px">
+        </nav>
+</header>
+

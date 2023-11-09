01c8f9a6d2576d38f0016190d5c318dcfdaa1b8e
theo <theoquennehenpro@gmail.com>
2023-11-09 11:30:23 +0100

    ajout anim js

--- JS/scroll.js
+++ JS/scroll.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         // Animation de défilement
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
+        }, 500, 'swing', function () {
             window.location.hash = target;
         });
     });
